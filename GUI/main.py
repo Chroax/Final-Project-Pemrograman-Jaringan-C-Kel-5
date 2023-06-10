3006d4d52be5450dc94321ac3ad3c00f09671cf8
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2023-06-11 03:35:01 +0700

    Add routing for sign-in & sign-up

---
+++ GUI/main.py
@@ -0,0 +1,22 @@
+from router import *
+
+def main(page : Page):
+    # Dimension
+    page.title = "ChatKui"
+    page.horizontal_alignment = 'center'
+    page.vertical_alignment = 'center'
+    page.bgcolor = '#1B202D'
+    page.fonts = {
+        "Poppins": "fonts/Poppins/Poppins-Bold.ttf",
+        "Mulish": "fonts/Mulish/Mulish-VariableFont_wght.ttf"
+    }
+
+    myRouter = Router(page)
+    page.on_route_change = myRouter.route_change
+
+    page.add(myRouter.body)
+    # myRouter.animate_box()
+    page.update()
+
+if __name__ == "__main__":
+    flet.app(target=main, assets_dir='assets', view=WEB_BROWSER)
